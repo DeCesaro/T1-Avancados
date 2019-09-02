e9dc1eb71b64f9ce8014e7b58a15dd15ef5e76dc
DeCesaro <be.cesaro@gmail.com>
2019-09-02 17:46:09 -0300

    Finalizado e rodando, falta apenas implementar um tempo de execucao para poder analisar cada entrada depois

--- src/src/Main.java
+++ src/src/Main.java
@@ -19,9 +19,7 @@ public class Main {
         Random randomNum = new Random();
         int tamanho = numPar + numImpar;
         int[] array = new int[tamanho];
-        int countPar = 0;
-        int countImpar = 0;
-        int i = 0;
+        int countPar = 0, countImpar = 0, i = 0;
         while (i < tamanho) {
             int rand = randomNum.nextInt(100); //gera até x números randômicos
             if (rand % 2 == 0) {
@@ -43,38 +41,38 @@ public class Main {
      * Método que irá percorrer o vetor e agrupar os números pares no inicio e ímpares no final sem o uso de um vetor auxiliar.
      */
 
-        public static int[] ParImparSemVetor(int... array) {
+        public static int[] ParImparSemVetor(int... vet) { //Indica um número indeterminado de inteiros a serem colocados no array
             int i = 0; //1 iteracão
-            while (i < array.length) {
+            while (i < vet.length) {
                 boolean troca = false;
-                if (array[i] % 2 == 0) { //Verifica se é par
+                if (vet[i] % 2 == 0) { //Verifica se é par
                     i++;
                 } else {
                     int j = i;
                     while (!troca) {
-                        if (array[j] % 2 != 0 && j < array.length - 1) { //Verifica se é impar e incrementa o j enquanto ele nao for o último
+                        if (vet[j] % 2 != 0 && j < vet.length - 1) { //Verifica se é impar e incrementa o j enquanto ele nao for o último
                             j++;
                         } else { // Realiza a troca entre os pares e impares
-                            int aux = array[i];
-                            array[i] = array[j];
-                            array[j] = aux;
+                            int aux = vet[i];
+                            vet[i] = vet[j];
+                            vet[j] = aux;
                             i++;
                             troca = true;
                         }
                     }
                 }
             }
-            return array;
+            return vet;
         }
 
         /**
          * Método irá agrupar os pares no inicio e ímpares no final com o uso de um vetor auxiliar
          */
-        public static int[] ParImparComVetor(int... array) {
-            int[] ordenaVet = new int[array.length];
+        public static int[] ParImparComVetor(int... vet) {
+            int[] ordenaVet = new int[vet.length];
             int quantPar = 0;
-            int quantImpar = array.length - 1;
-            for (int num : array) {
+            int quantImpar = vet.length - 1;
+            for (int num : vet) {
                 if (num % 2 == 0) {
                     ordenaVet[quantPar] = num;
                     quantPar++;
